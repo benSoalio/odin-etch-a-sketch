74a4d75c2218e7c3765cccd6632bc13ddb9f198f
bsoalio <bsoalio@gmail.com>
2022-06-01 21:42:56 +0000

    Set up random color function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const pickedSize = document.querySelector("#pickedSize");
 const size = document.querySelector("#size");
 const colorPicker = document.querySelector("#colorPicker");
 const erase = document.querySelector("#erase");
+const randomColor = document.querySelector("#randomColor");
 
 // function to draw the grid with size as input
 function drawGrid(size) {
@@ -31,28 +32,50 @@ function drawGrid(size) {
 
 // Draw the grid depend on the size selected
 pickedSize.addEventListener("change", () => {
+  randomColor.classList.remove("active");
+  erase.classList.remove("active");
   size.textContent = `${pickedSize.value} X ${pickedSize.value}`;
   drawGrid(pickedSize.value);
 });
 
 //Setup the erase button
 erase.addEventListener("click", (e) => {
+  //deactivate the random
+  randomColor.classList.remove("active");
+  // if erase button non activate, activate it
   e.target.classList.toggle("active");
-  //   console.log(erase.getAttribute("class"));
 });
 
+// setup the random button
+randomColor.addEventListener("click", (e) => {
+  //deactivate the erase button
+  erase.classList.remove("active");
+  //if random button non activate, activate it
+  e.target.classList.toggle("active");
+});
+//Color picker event listener
+colorPicker.addEventListener("click", () => {
+  //deactivate the erase button
+  erase.classList.remove("active");
+  //deactivate the random
+  randomColor.classList.remove("active");
+});
 //Change the color of the square on mouseover
 sketchPad.addEventListener("mouseover", (e) => {
   // to prevent the change of the background of the sketchpad
   if (e.target !== sketchPad) {
-    // console.log(erase.getAttribute("class"));
     //change the background color of the square with the color picked
     e.target.style.backgroundColor = colorPicker.value;
     // if the erase button is active
     if (erase.getAttribute("class") === "active") {
       // remove the background color
       e.target.style.backgroundColor = "#ffffff";
-      //console.log(e.target);
+    }
+    if (randomColor.getAttribute("class") === "active") {
+      let rValue = Math.floor(Math.random() * 255);
+      let gValue = Math.floor(Math.random() * 255);
+      let bValue = Math.floor(Math.random() * 255);
+      e.target.style.backgroundColor = `rgb(${rValue},${gValue},${bValue})`;
     }
   }
 });
